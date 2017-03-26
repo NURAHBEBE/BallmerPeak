b15943a5d26214569f56be229f7fa659d0a187fc
Neil Sekhon <nsekhon.96@gmail.com>
2017-03-26 05:38:48 -0400

    github commit

--- BallmerPeak/ViewController.swift
+++ BallmerPeak/ViewController.swift
@@ -23,13 +23,17 @@ class ViewController: UIViewController {
     
     
     @IBOutlet weak var calculateButton: UIButton!
-    @IBOutlet weak var helpButton: UIButton!
     
     @IBAction func drinkStepperChanged(_ sender: Any) {
-        numDrinksLabel.text = (drinksStepper.value == 1) ? "1 drink" : "\(drinksStepper.value) drinks"
+        numDrinksLabel.text = "Number of drinks you drank: \(Int(drinksStepper.value))"
         
         if drinksStepper.value == 10 && showSavage {
-            showAlert(self, title: "Savage", message: "", okMessage: "I know")
+            showAlert(self, title: "Savage", message: "", okMessage: "Ok")
+            showSavage = false
+        }
+        
+        if drinksStepper.value == 25 && showHospital {
+            showAlert(self, title: "Hospital time", message: "Please go to the hospital", okMessage: "Ok")
             showSavage = false
         }
     }
@@ -39,7 +43,16 @@ class ViewController: UIViewController {
     @IBOutlet weak var weightSlider: UISlider!
     
     @IBAction func weightSliderChanged(_ sender: Any) {
-        weightLabel.text = "Weight: \(weightSlider.value) lbs"
+        weightLabel.text = "Weight: \(Int(weightSlider.value)) lbs"
+    }
+    
+    @IBOutlet weak var bacLabel: UILabel!
+    
+    @IBOutlet weak var bacSlider: UISlider!
+    
+    @IBAction func bacSliderChanged(_ sender: Any) {
+
+        bacLabel.text = ("\(ballmerText()) BAC: \(Double(bacSlider.value).roundTo(places: 4)) %")
     }
     
     
@@ -50,6 +63,7 @@ class ViewController: UIViewController {
     let startDate = Date()
     
     var showSavage = true
+    var showHospital = true
     
     // OVERRIDE FUNCS----------------------------------
     override func viewDidLoad() {
@@ -58,22 +72,34 @@ class ViewController: UIViewController {
         segmentedControl.selectedSegmentIndex = 0
         
         calculateButton.addTarget(self, action: #selector(ViewController.calculatePressed), for: .touchUpInside)
-        helpButton.addTarget(self, action: #selector(ViewController.showHelp), for: .touchUpInside)
+        //helpButton.addTarget(self, action: #selector(ViewController.showHelp), for: .touchUpInside)
+        
+        firstDrinkPicker.countDownDuration = 1
+        lastDrinkPicker.countDownDuration = 1
+    }
+    
+    func ballmerText() -> String {
+        return (bacSlider.value > 0.12 && bacSlider.value < 0.14) ? "Ballmer" : "Custom"
     }
     
     // todo: show savage alert if more than 10 drinks
-    
+    /*
     func showHelp() {
-        let imageView = UIImageView(frame: CGRect(x: 50, y: 100, width: 200, height: 100))
-        imageView.image = UIImage(named: "just_drinks_for_web3")
-        
+        showAlert(self, title: "Standard drinks (alchohol %)", message: "1 can of beer (5%)\n", okMessage: "")
+        /*
         let alert = UIAlertController(title: nil, message: nil, preferredStyle: UIAlertControllerStyle.alert)
+        
+        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 400, height: 200))
+        imageView.image = UIImage(named: "just_drinks_for_web3")
+
         alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.default, handler: nil))
         
-        alert.view.addSubview(imageView)
-        
         self.present(alert, animated: true, completion: nil)
+        
+        alert.view.addSubview(imageView)
+        */
     }
+    */
     
     func calculatePressed() {
         // get input vals
@@ -90,8 +116,8 @@ class ViewController: UIViewController {
         let isMale = (segmentedControl.selectedSegmentIndex == 0)
         
         
-        let hoursSinceFirst = Double(firstDrinkPicker.date.minutes(from: startDate) * 60)
-        let hoursSinceLast = Double(lastDrinkPicker.date.minutes(from: startDate) * 60)
+        let hoursSinceFirst = Double(firstDrinkPicker.countDownDuration/3600)
+        let hoursSinceLast = Double(lastDrinkPicker.countDownDuration/3600)
         
         if hoursSinceFirst < hoursSinceLast {
             showAlert(self, title: "Try Again", message: "You couldn't have had your last drink before your first drink", okMessage: "Sorry")
@@ -126,7 +152,7 @@ class ViewController: UIViewController {
     
     // calculate the time it will take to reach the Ballmer peak
     func calcTime(_ currBac: Double, hoursSinceFirst: Double, constantDrinks: Double) {
-        let hours = (currBac - 0.1337)/0.015
+        let hours = (currBac - Double(bacSlider.value))/0.015
         
         // add in seconds
         let date = startDate.addingTimeInterval(hours * 3600.0)
@@ -134,17 +160,21 @@ class ViewController: UIViewController {
         if date < startDate {
             let startedDrinkingDate = startDate.addingTimeInterval(-(hoursSinceFirst * 3600.0))
             if date < startedDrinkingDate {
-                showAlert(self, title: "Warning", message: "You will not reach Ballmer Peak, please consume more alcohol", okMessage: "Ok")
+                showAlert(self, title: "Warning", message: "You will not reach \(ballmerText()) Peak. \nPlease consume more alcohol", okMessage: "Ok")
                 return
             }
             
-            showAlert(self, title: "Warning", message: "You have already peaked (Ballmer-wise), please consume more alcohol", okMessage: "Ok")
+            showAlert(self, title: "Warning",
+                      message: "You have already peaked (\(ballmerText())-wise) at \(date.toShortTimeString()). \nPlease consume more alcohol",
+                okMessage: "Ok")
+            
+            return
             
         }
         
-        showAlert(self, title: "Ballmer Peak Report:",
-            message: "You will Ballmer peak at \(date.toShortTimeString()). At this time, have \(constantDrinks) drinks every hour to maintain your peak",
-            okMessage: "Wow thanks!")
+        showAlert(self, title: "\(ballmerText()) Peak Report:",
+            message: "You will \(ballmerText()) peak at \(date.toShortTimeString()). At this time, have \(constantDrinks) drinks every hour to maintain your peak",
+            okMessage: "Ok")
     }
 
 }
@@ -155,6 +185,8 @@ func showAlert(_ vc: UIViewController, title: String, message: String, okMessage
     
     vc.present(alert, animated: true, completion: nil)
 }
+
+
 
 extension Double {
     /// Rounds the double to decimal places value
